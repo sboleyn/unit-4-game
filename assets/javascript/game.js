fd14e7c15fd7982791f13c9b5bc22bc761594b64
Sami B <sboleyn@email.arizona.edu>
2018-08-14 17:11:55 -0700

    characters go to the right place when clicked

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+// $(document).ready(function() {
+    var isFighterChosen = false;
+    var isDefenderChosen = false;
+
+    function initializeGame (){
+        isFighterChosen = false;
+    }
+
+    $(".character").on("click", function() {
+        //check if a character is already selected then another click will send this guy to defender
+        if (isFighterChosen){
+            return false;
+        }
+    
+        $(this).css("backgroundColor", "#66cccc");
+        isFighterChosen = true;
+        
+        $(".character").not(this).appendTo($("#enemies"));
+
+        $(".character").not(this).css("backgroundColor", "red");        
+
+        $(".character").not(this).addClass("defenders");  
+        
+        $(".defenders").on("click", function() {
+
+            if (isDefenderChosen){
+                return false;
+            }
+    
+            $(this).css("backgroundColor", "gray");
+            
+            isDefenderChosen = true;
+            
+            $(this).appendTo($("#defender"));
+        })
+    });
+
+    
+
+// })
